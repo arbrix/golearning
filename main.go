4e94d45817c18c48840563186daf3721a278d16d
opast <opast@softserveinc.com>
2017-04-18 19:31:19 +0300

    First program

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
-package golearning
+package main
+import "time"
+import "fmt"
+func main() {
+	location, _ := time.LoadLocation("Europe/Kiev")
+	fmt.Println("Local time in Kiev: ", time.Now().In(location))
+
+	location, _ = time.LoadLocation("Europe/Budapest")
+	fmt.Println("Local time in Budapest: ", time.Now().In(location))
+
+	location, _ = time.LoadLocation("America/Los_Angeles")
+	fmt.Println("Local time in LosAngeles: ", time.Now().In(location))
+}
