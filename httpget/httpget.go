8674c34db287a3690073c1c5e6326a3b88d37a31
opast <opast@softserveinc.com>
2017-04-20 18:23:49 +0300

    httpget script

--- httpget/httpget.go
+++ httpget/httpget.go
@@ -1,1 +1,28 @@
-package httpget
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main()  {
+
+	URL := os.Args[1]
+//	URL := "http://httpbin.org/ip"
+
+	resp, err := http.Get(URL)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(body))
+}
